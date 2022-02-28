00b14e78a1be98978786d807c5bd0b5c83f8c8e5
John Edisson Ortiz <j.ortiz3109@gmail.com>
2022-02-28 15:05:12 -0500

    style: pHP CS FIXER

--- database/migrations/2022_01_24_222348_create_expenses_table.php
+++ database/migrations/2022_01_24_222348_create_expenses_table.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateExpensesTable extends Migration
 {
-
     public function up(): void
     {
         Schema::create('expenses', function (Blueprint $table) {
