99252e0d918c34ecf9990653b1b13bfd22ba7488
John Edisson Ortiz Román <j.ortiz3109@gmail.com>
2022-08-12 01:09:41 -0500

    refactor: Refactored adminstration translations

---
+++ resources/lang/en/admin.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'incomes' => [
+        'module' => [
+            'title' => 'Administration of incomes',
+        ],
+        'messages' => [
+            'created' => 'Income created successfully',
+        ]
+    ],
+];
