abede9974eb1e5e761838c8dea5e8eb9fce07718
John Edisson Ortiz Román <j.ortiz3109@gmail.com>
2022-08-12 01:02:26 -0500

    build: Added bootstrap framework

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,10 @@
-window._ = require('lodash');
+import _ from 'lodash';
+window._ = _;
+
+import * as Popper from '@popperjs/core'
+window.Popper = Popper
+
+import 'bootstrap'
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -6,7 +12,8 @@ window._ = require('lodash');
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
+import axios from "axios";
+window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,18 +3,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <title>{{ config('app.name', 'Laravel') }} @isset($moduleTitle) - {{ $moduleTitle }}@endisset</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-    <main class="py-4">
+    <main class="py-4" id="app">
         @yield('content')
     </main>
 </body>
