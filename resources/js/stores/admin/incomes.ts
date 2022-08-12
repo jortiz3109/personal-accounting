f84d49b27e00be26a39b2bfc0abe6ba6f2d3b007
John Edisson Ortiz Román <j.ortiz3109@gmail.com>
2022-08-12 16:40:05 -0500

    feat: Added store for incomes

---
+++ resources/js/stores/admin/incomes.ts
@@ -0,0 +1,22 @@
+import {defineStore} from 'pinia'
+import {Income} from '../../classes/Income'
+
+export const useIncomeStore = defineStore('incomes', {
+    state: () => ({
+        incomes: [] as Income[]
+    }),
+    actions: {
+        all: function(): Income[] {
+            return this.incomes
+        },
+        add: function(income: Income) {
+            this.incomes.push(income)
+        },
+        total: function (): number {
+            return this.incomes.length
+        },
+        reset: function (): void {
+            this.$reset()
+        }
+    }
+})
